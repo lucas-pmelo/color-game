721352dc99a004f6f25074d7fbbdf3b0b5cd985b
lucas-pmelo <lucas.pinheiro.melo@gmail.com>
2022-10-06 18:10:25 -0300

    Base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [result, setResult] = useState<boolean | undefined>(undefined);
+
+  const randomColor = () => {
+    const colors = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F"
+    ];
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += colors[Math.floor(Math.random() * colors.length)];
+    }
+    return color;
+  };
+
+  const handleAnswerClicked = (answer: string) => {
+    if (answer === color) {
+      setResult(false);
+      pickColor();
+    } else {
+      setResult(true);
+    }
+  };
+
+  const pickColor = () => {
+    const actualColor = randomColor();
+    setColor(actualColor);
+    console.log(actualColor);
+    setAnswers(
+      [actualColor, randomColor(), randomColor()].sort(
+        () => Math.random() - 0.5
+      )
+    );
+  };
+
+  useEffect(() => {
+    pickColor();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="panel" style={{ background: color }}></div>
+        <div className="answers">
+          {answers.map((answer) => (
+            <button onClick={() => handleAnswerClicked(answer)} key={answer}>
+              {answer}
+            </button>
+          ))}
+          {result === false && <p className="correct">Correct answer!</p>}
+          {result === true && <p className="wrong">Wrong answer!</p>}
+        </div>
+      </div>
     </div>
   );
 }
